2269b6302b75f543d863691fb06b3b7aa3cbdda0
josephmathew900 <josephmathew900@gmail.com>
2021-02-18 22:16:20 +0530

    deleted aricles table from db using a migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_18_162035) do
-
-  create_table "articles", force: :cascade do |t|
-    t.string "name"
-    t.text "desc"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ActiveRecord::Schema.define(version: 2021_02_18_164340) do
 
   create_table "tasks", force: :cascade do |t|
     t.text "title"
