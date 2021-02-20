37cfffd7acaa4bd7dc36553767cc83d53a204362
josephmathew900 <josephmathew900@gmail.com>
2021-02-20 19:42:58 +0530

    Added ability to update a task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
 
-  before_action :load_task, only: [:show]
+  before_action :load_task, only: [:show, :update]
   
   def index
     tasks = Task.all
@@ -19,6 +19,14 @@ class TasksController < ApplicationController
 
   def show
     render status: :ok, json: {task: @task}
+  end
+
+  def update
+    if @task.update(task_params)
+      render status: :ok, json: { notice: 'Successfully updated task.' }
+    else
+      render status: :unprocessable_entity, json: { errors: @task.errors.full_messages }
+    end
   end
 
   private
--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -1,11 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { initializeLogger } from "common/logger";
-import Dashboard from "components/Dashboard";
-import CreateTask from "components/Tasks/CreateTask";
-import ShowTask from "components/Tasks/ShowTask";
 import { registerIntercepts, setAuthHeaders } from "apis/axios";
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
+
+
+import Dashboard from "components/Dashboard";
+import CreateTask from "components/Tasks/CreateTask";
+import ShowTask from "components/Tasks/ShowTask";
+import EditTask from "components/Tasks/EditTask";
+
+
+
+
 
 const App = () => {
   const [loading, setLoading] = useState(true);
@@ -20,6 +27,7 @@ const App = () => {
     <Router>
       <ToastContainer />
       <Switch>
+        <Route exact path="/tasks/:id/edit" component={EditTask} />
         <Route exact path="/tasks/:id/show" component={ShowTask} />
         <Route exact path="/tasks/create" component={CreateTask} />
         <Route exact path="/dashboard" component={Dashboard} />
