bd916ee17baece46ea06375cc4033fbff73faf13
josephmathew900 <josephmathew900@gmail.com>
2021-02-21 20:35:27 +0530

    Added login feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,12 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      render status: :ok, json: { notice: t('successfully_created', entity: 'Task') }
+    else
+      render status: :unprocessable_entity, json: { errors: @user.errors.full_messages.to_sentence }
+    end
   end
 
   private
